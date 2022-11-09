3dcd230ff409d0377a8da9376cb48d71452a606f
ZiedBoutiti <ziedboutiti2@gmail.com>
2022-11-09 14:24:12 +0100

    init

---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: must_be_immutable
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  CustomTextField(
+      {super.key,
+      this.hintText,
+      this.inputType,
+      this.onChanged,
+      this.obscureText = false});
+  Function(String)? onChanged;
+  String? hintText;
+  TextInputType? inputType;
+
+  bool? obscureText;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: obscureText!,
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        hintText: hintText,
+        enabledBorder: OutlineInputBorder(
+          borderSide: const BorderSide(),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        border: OutlineInputBorder(
+          borderSide: const BorderSide(),
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    );
+  }
+}
